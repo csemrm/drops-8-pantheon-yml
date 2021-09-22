a39c48d06655f9967d6e0ac470640a9937c0ebfd
Mostafizur Rahman <csemrm@gmail.com>
2021-09-22 10:07:55 +0600

    Update drush_site_install.php

--- private/scripts/deploy_product/drush_site_install.php
+++ private/scripts/deploy_product/drush_site_install.php
@@ -1,17 +1,3 @@
 <?php
 // create dummy file to set site-install existing config folder.
-$path = $_SERVER['DOCROOT'] . "quicksilver.txt";
-$file = fopen($path, 'w') or die('Error creating Quicksilver dummy file');
-fclose($file);
-
-print "Installing Drupal...\n";
-passthru('drush site-install standard --existing-config -y');
-print "Install Drupal complete.\n";
-
-// remove dummy file.
-unlink($path);
-
-//  Config export.
-echo "Exporting configuration...\n";
-passthru('drush config-export -y');
-echo "Export of configuration complete.\n";
+echo $_SERVER['DOCROOT'] . "quicksilver.txt";
